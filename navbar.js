8cd8607672a36b58c43a0cc51921d010c81567cc
arnoldabigail <arnold.abigail@auf.edu.ph>
2024-12-14 02:42:06 +0800

    navbar.js

--- navbar.js
+++ navbar.js
@@ -7,12 +7,10 @@ class NavbarComponent extends HTMLElement {
             <ul class="nav_links">
                 <li><a href="index.html">Home</a></li>
                 <li><a href="products.html">Products</a></li>
-                <li><a href="featured.html">Featured</a></li>
-                <li><a href="deals.html">Deals</a></li>
                 <li><a href="about.html">About</a></li>
             </ul>
         </nav>
-        <a href="cart.html" class="cart"><img src="shopping-cart.png" id="carticon">Cart</a>
+        <a href="cart.html" class="cart"><img src="shopping-cart2.png" id="carticon">Cart</a>
     </div>
       `;
     }
